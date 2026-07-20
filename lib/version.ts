6b2c8809b47936b45782e51e4db3731089cbd4b3
lerk-dev <lerk-dev@example.com>
2026-07-20 23:59:25 +0800

    chore: bump version to 0.2.146

--- lib/version.ts
+++ lib/version.ts
@@ -1,10 +1,10 @@
 // 自动生成的版本信息 - 请勿手动修改
-// 生成时间: 2026/7/20 22:45:15
+// 生成时间: 2026/7/20 23:58:32
 
-export const VERSION = '0.2.145'
-export const BUILD_ID = '20260720.224515'
-export const BUILD_DATE = '2026-07-20T14:45:15.126Z'
-export const BUILD_DATE_LOCAL = '2026/7/20 22:45:15'
+export const VERSION = '0.2.146'
+export const BUILD_ID = '20260720.235832'
+export const BUILD_DATE = '2026-07-20T15:58:32.068Z'
+export const BUILD_DATE_LOCAL = '2026/7/20 23:58:32'
 
 export function getVersionInfo() {
   return {
